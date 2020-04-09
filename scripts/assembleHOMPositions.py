cd832d13e316f7afc967fcadc3d33633b43b0939
Philipp Spohr <philipp.spohr@hhu.de>
2020-04-09 19:03:00 +0200

    output allele emission frequencies

--- scripts/assembleHOMPositions.py
+++ scripts/assembleHOMPositions.py
@@ -3,6 +3,7 @@ from shared import *
 import json
 
 positions = []
+emissionProbabilities = {}
 
 for labelFilePath in snakemake.input:
 	with open(labelFilePath,'r') as infile:
@@ -12,6 +13,14 @@ for labelFilePath in snakemake.input:
 				if labels[position][allele][0] == "HOM":
 					if not position in positions:
 						positions.append(position)
+						emissionProbabilities[position] = []
+					emissionProbabilities[position].append(labels[position][allele][1])
+
+output = {
+	position : (sum(emissionProbabilities[position])/len(emissionProbabilities[position]),emissionProbabilities[position])
+	for position in positions
+}
+
 
 with open(snakemake.output[0],'w') as outfile:
-	json.dump(positions,outfile)
+	json.dump(output,outfile)
