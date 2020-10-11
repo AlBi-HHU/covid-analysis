b352e243c7602140ce8b4c41dfaa588e76a89929
Philipp Spohr <philipp.spohr@hhu.de>
2020-10-11 21:45:23 +0200

    check for nonexisting manual curation seq

--- scripts/vcfdiff_summary_manual.py
+++ scripts/vcfdiff_summary_manual.py
@@ -1,5 +1,6 @@
 from shared import *
 import vcfpy
+import os.path
 
 
 def altToText(ALT):
@@ -27,7 +28,7 @@ with open(snakemake.output[0],'w') as outfile:
         pancov_vcf = next(iterator)
         pileup = parsePileup(next(iterator))
 
-        if comparison_table == '':
+        if comparison_table == '' or not os.path.exists(comparison_table):
             #TODO: Log Debug Output? ...
             print('No manual curation (GISAID Seq) found for: {}'.format(oancov_vcf))
             continue
