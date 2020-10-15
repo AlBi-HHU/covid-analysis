f9de75099761a45be321a12cdee2db11d5ea9d3b
Philipp Spohr <philipp.spohr@hhu.de>
2020-10-15 15:27:53 +0200

    readd header line

--- scripts/pangenome/concate_vcf.py
+++ scripts/pangenome/concate_vcf.py
@@ -10,7 +10,7 @@ def main(out_path, vcfs):
     header.add_contig_line({"ID": "MN908947.3", "length": "29903"})
     header.add_filter_line({'Description': "All filters passed", 'ID': 'PASS'})
     header.add_filter_line({'Description': "dp", 'ID': 'dp'})
-    #header.add_line(vcfpy.SampleHeaderLine.from_mapping({'ID':'all_var'}))
+    header.add_line(vcfpy.SampleHeaderLine.from_mapping({'ID':'all_var'}))
     
     writer = vcfpy.Writer.from_path(out_path, header)
 
