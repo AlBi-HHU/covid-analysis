4edf38bda3e073ef644aafd0d4572848b2a28c1a
Pierre Marijon <pierre.marijon@hhu.de>
2020-10-20 18:01:31 +0200

    Variant with same position but with different REF and ALT isn't squash in same variant

--- scripts/pangenome/clean_variant.py
+++ scripts/pangenome/clean_variant.py
@@ -11,20 +11,22 @@ def main(in_vcf, min_cov, out_vcf):
     pos2var = defaultdict(list)
     for record in reader:
         coverage = record.INFO["VCOV"] + record.INFO["RCOV"]
-        pos2var[record.POS].append((coverage, record))
+        pos2var[(record.POS, tuple(record.REF), tuple(record.ALT))].append((coverage, record))
 
     header.add_info_line({"ID": "MULTIPLE", "Type": "Flag", "Number": "1", "Description": "Pangenome found multiple variant at this position"})
     writer = vcfpy.Writer.from_path(out_vcf, reader.header)
     
     for key, values in pos2var.items():
         if len(values) == 1:
+            coverage = values[0][0]
             variant = values[0][1]
         else:
             values.sort(key=lambda x: x[0], reverse=True)
+            coverage = values[0][0]
             variant = values[0][1]
             variant.INFO["MULTIPLE"] = True
 
-        if variant.INFO["VCOV"] + variant.INFO["RCOV"] > min_cov:
+        if coverage > min_cov:
             writer.write_record(variant)
 
 
