072477a5a15068dc81ea3d1bc02bc0a46a77456a
Philipp Spohr <philipp.spohr@hhu.de>
2021-01-20 13:18:53 +0100

    add aggregation script for recovs

---
+++ scripts/aggregateRecoveries.py
@@ -0,0 +1,18 @@
+
+with open(snakemake.output[0],'w') as outfile:
+	total = 0
+	rejected = 0
+	for f in snakemake.input:
+		outfile.write(f+'\n')
+		with open(f,'r') as infile:
+			ll = infile.read().splitlines()[1:]
+			for l in ll:
+				outfile.write(l+'\n')
+				d = l.split()
+				reject = eval(d[3])
+				if reject:
+					rejected += 1
+				total += 1
+
+	outfile.write('rejected {} of {} found variants'.format(rejected,total))
+
