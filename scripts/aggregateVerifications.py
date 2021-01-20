10028ad6adaa8dee3bac16dd130b76bf8a972708
Philipp Spohr <philipp.spohr@hhu.de>
2021-01-20 12:18:53 +0100

    fixi

--- scripts/aggregateVerifications.py
+++ scripts/aggregateVerifications.py
@@ -3,14 +3,16 @@ with open(snakemake.output[0],'w') as outfile:
 	total = 0
 	verified = 0
 	for f in snakemake.input:
+		outfile.write(f+'\n')
 		with open(f,'r') as infile:
 			ll = infile.read().splitlines()
 			for l in ll:
+				outfile.write(l)
 				d = l.split()
 				reject = d[3]
 				if not reject:
 					verified += 1
 				total += 1
 
-	outfile.write(total,verified)
+	outfile.write('{},{}'.format(total,verified))
 
