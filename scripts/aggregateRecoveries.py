bebd1972a64fce26b29cab34c95461002c192211
Philipp Spohr <philipp.spohr@hhu.de>
2021-01-20 13:19:05 +0100

    ii

--- scripts/aggregateRecoveries.py
+++ scripts/aggregateRecoveries.py
@@ -1,7 +1,7 @@
 
 with open(snakemake.output[0],'w') as outfile:
 	total = 0
-	rejected = 0
+	recoveredCount = 0
 	for f in snakemake.input:
 		outfile.write(f+'\n')
 		with open(f,'r') as infile:
@@ -9,10 +9,12 @@ with open(snakemake.output[0],'w') as outfile:
 			for l in ll:
 				outfile.write(l+'\n')
 				d = l.split()
-				reject = eval(d[3])
-				if reject:
-					rejected += 1
+				recovered = eval(d[3])
+				if recovered == -1:
+					continue
+				elif recovered == True:
+					recoveredCount += 1
 				total += 1
 
-	outfile.write('rejected {} of {} found variants'.format(rejected,total))
+	outfile.write('recovered {} of {} found variants'.format(recoveredCount,total))
 
