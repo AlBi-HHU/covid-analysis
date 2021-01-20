632c5fc8ee65e162d7228a230c8a61a382651b47
Philipp Spohr <philipp.spohr@hhu.de>
2021-01-20 12:25:09 +0100

    better stats

--- scripts/aggregateVerifications.py
+++ scripts/aggregateVerifications.py
@@ -1,7 +1,7 @@
 
 with open(snakemake.output[0],'w') as outfile:
 	total = 0
-	verified = 0
+	rejected = 0
 	for f in snakemake.input:
 		outfile.write(f+'\n')
 		with open(f,'r') as infile:
@@ -9,10 +9,10 @@ with open(snakemake.output[0],'w') as outfile:
 			for l in ll:
 				outfile.write(l+'\n')
 				d = l.split()
-				reject = d[3]
-				if not reject:
-					verified += 1
+				reject = bool(d[3])
+				if reject:
+					rejected += 1
 				total += 1
 
-	outfile.write('{},{}'.format(total,verified))
+	outfile.write('rejected {} of {} found variants'.format(rejected,total))
 
