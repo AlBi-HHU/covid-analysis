89d1a35938b799c02b0acf1863eedbc74ae2d836
Philipp Spohr <philipp.spohr@hhu.de>
2021-01-28 10:01:38 +0100

    pierreplots

---
+++ scripts/plotCoveragePerVariant.py
@@ -0,0 +1,43 @@
+import os
+from shared import *
+import pandas as pd
+
+print('Identifying var positions ...')
+
+positions = set()
+with open(snakemake.input['verification'],'r') as varfile:
+	for l in varfile.read().splitlines():
+		d = l.split('\t')
+		if len(d) > 1:
+			positions.add(d[0])
+		else:
+			continue
+
+print('Collecting data ...')
+
+tuples = []
+
+
+for f in snakemake.input['iteratorList']:
+	pileup = parsePileupStrandAwareLight(f)
+	for pos in pileup:
+		if pos in positions:
+			tuples.append((pos,f,sum(pileup[pos].values())))
+
+df = pd.DataFrame(tuples,columns=['pos','file','cov'])
+print('plotting')
+
+for pos in positions:
+	base = alt.Chart(df[df.pos == pos])
+
+	bar = base.mark_bar().encode(
+		x=alt.X('cov:Q', bin=True),
+		y='count()'
+	)
+
+	rule = base.mark_rule(color='red').encode(
+		x='mean(cov):Q',
+		size=alt.value(5)
+	)
+	os.makedirs(snakemake.output[0])
+	(bar + rule).save(os.path.join(snakemake.output[0],'{}.cov.html'.format(pos)))
