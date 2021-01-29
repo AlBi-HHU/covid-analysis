9ff2b480529f588b5e0bb873c8f5eef49a8c8cc6
Pierre Marijon <pierre.marijon@hhu.de>
2021-01-29 14:05:46 +0100

    Include RVT in top level stats

--- scripts/topLevelStats.py
+++ scripts/topLevelStats.py
@@ -1,19 +1,18 @@
 with open(snakemake.input['verification'],'r') as verFile,open(snakemake.input['recovery'],'r') as recFile,open(snakemake.output[0],'w') as outfile:
 
-	vd = verFile.read().splitlines()[-1].split()
+    vd = verFile.read().splitlines()[-1].split()
 
-	falsePositive = int(vd[1])
-	positive = int(vd[3])
-	truePositive = positive-falsePositive
-	precision =truePositive/positive
+    falsePositive = int(vd[1])
+    positive = int(vd[3])
+    truePositive = positive-falsePositive
+    precision =truePositive/positive
 
-	rd = recFile.read().splitlines()[-1].split()
+    rd = recFile.read().splitlines()[-1].split()
 
-	realPositives = int(rd[3])
-	recovered = int(rd[1])
-	recall = recovered/realPositives
+    realPositives = int(rd[3])
+    recovered = int(rd[1])
+    recall = recovered/realPositives
 
-	f1 = 2*(precision*recall)/(precision+recall)
+    f1 = 2*(precision*recall)/(precision+recall)
 
-	outfile.write('step:{},precision:{},recall:{},f1:{}'.format(snakemake.config['pangenomeMinCovFactor'],precision,recall,f1))
-
+    print(f"{snakemake.config['pangenomeMinCovFactor']},{snakemake.config['pangenomeRVTThreshold']},{precision},{recall},{f1}", file=outfile)
