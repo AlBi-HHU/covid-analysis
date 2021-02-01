e6e86c3328074507da35b595b08426138ad99348
Philipp Spohr <spohr@brandt.albi.uni-duesseldorf.de>
2021-02-01 13:59:48 +0100

    latest changes on brandt

--- scripts/hetPlot1.py
+++ scripts/hetPlot1.py
@@ -2,26 +2,49 @@ import sys
 import altair as alt
 import pandas as pd
 import vcfpy
+from functools import reduce
 
 tuples = []
 
 ambiguousLetters = ['R','Y','S','W','K','M']
-
-for f in snakemake.input:
-    reader = vcfpy.Reader.from_path(f)
+iterator = iter(snakemake.input)
+for pancovF,ivarF in zip(snakemake.input['pancov'],snakemake.input['ivar']):
+    reader = vcfpy.Reader.from_path(pancovF)
     for record in reader:
         isHet = False
+        #print(record.ALT[0].value)
         for l in ambiguousLetters:
-            if l in record.ALT:
+            if l in record.ALT[0].value:
                 isHet = True
                 break
         if isHet:
-            tuples.append((f,record.POS,record.INFO['RVT']))
+            alleleFrequency = record.INFO['VCOV']/(record.INFO['VCOV']+record.INFO['RCOV'])
+            tuples.append((pancovF,record.POS,'pancov',alleleFrequency))           
+            ivartable = open(ivarF,'r').read().splitlines()[1:]
+            illuminafreq = -1
+            for il in ivartable:
+                d = il.split()
+                print(d)
+                if int(d[1]) == int(record.POS):
+                    illuminafreq = float(d[9])
+                    tuples.append((pancovF,record.POS,'illumina',illuminaFreq))
+                    break
+            
 
-df = pd.DataFrame(tuples,columns=['file','pos','rvt'])
+    #break #remove
 
-chart = alt.Chart(df).mark_rect().encode(
-    y = 'pos:O',
-    x = 'file:N',
-    color= 'rvt:Q'
-).interactive().save(snakemake.output[0])
+df = pd.DataFrame(tuples,columns=['file','pos','method','rvt'])
+
+charts = []
+
+for f in df['file'].unique():
+
+    charts.append(alt.Chart(df[df.file == f],title=f).mark_rect().encode(
+        y = 'pos:O',
+        x = alt.X('rvt:Q',scale=alt.Scale(domain=[0,1])),
+        color= 'method:N',
+        tooltip = ['rvt']
+    ).interactive())
+
+concat = reduce(alt.vconcat,charts)
+concat.save(snakemake.output[0])
