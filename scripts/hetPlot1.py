a5a2beaca48e7d6970ca0abe69012e1f963ce191
Pierre Marijon <pierre.marijon@hhu.de>
2021-02-01 18:42:25 +0100

    Fix typo in hetPlot script

--- scripts/hetPlot1.py
+++ scripts/hetPlot1.py
@@ -6,45 +6,53 @@ from functools import reduce
 
 tuples = []
 
-ambiguousLetters = ['R','Y','S','W','K','M']
+ambiguousLetters = ["R", "Y", "S", "W", "K", "M"]
 iterator = iter(snakemake.input)
-for pancovF,ivarF in zip(snakemake.input['pancov'],snakemake.input['ivar']):
+
+for pancovF, ivarF in zip(snakemake.input["pancov"], snakemake.input["ivar"]):
     reader = vcfpy.Reader.from_path(pancovF)
     for record in reader:
         isHet = False
-        #print(record.ALT[0].value)
+
         for l in ambiguousLetters:
             if l in record.ALT[0].value:
                 isHet = True
                 break
         if isHet:
-            alleleFrequency = record.INFO['VCOV']/(record.INFO['VCOV']+record.INFO['RCOV'])
-            tuples.append((pancovF,record.POS,'pancov',alleleFrequency))           
-            ivartable = open(ivarF,'r').read().splitlines()[1:]
+            alleleFrequency = record.INFO["VCOV"] / (
+                record.INFO["VCOV"] + record.INFO["RCOV"]
+            )
+            tuples.append((pancovF, record.POS, "pancov", alleleFrequency))
+
+            ivartable = open(ivarF, "r").read().splitlines()[1:]
             illuminafreq = -1
             for il in ivartable:
                 d = il.split()
                 print(d)
                 if int(d[1]) == int(record.POS):
                     illuminafreq = float(d[9])
-                    tuples.append((pancovF,record.POS,'illumina',illuminaFreq))
+                    tuples.append((pancovF, record.POS, "illumina", illuminafreq))
                     break
-            
 
-    #break #remove
+    # break #remove
 
-df = pd.DataFrame(tuples,columns=['file','pos','method','rvt'])
+df = pd.DataFrame(tuples, columns=["file", "pos", "method", "rvt"])
 
 charts = []
 
-for f in df['file'].unique():
+for f in df["file"].unique():
 
-    charts.append(alt.Chart(df[df.file == f],title=f).mark_rect().encode(
-        y = 'pos:O',
-        x = alt.X('rvt:Q',scale=alt.Scale(domain=[0,1])),
-        color= 'method:N',
-        tooltip = ['rvt']
-    ).interactive())
+    charts.append(
+        alt.Chart(df[df.file == f], title=f)
+        .mark_rect()
+        .encode(
+            y="pos:O",
+            x=alt.X("rvt:Q", scale=alt.Scale(domain=[0, 1])),
+            color="method:N",
+            tooltip=["rvt"],
+        )
+        .interactive()
+    )
 
-concat = reduce(alt.vconcat,charts)
+concat = reduce(alt.vconcat, charts)
 concat.save(snakemake.output[0])
