dcca69c45602bc5842a2b722a1f9975da8d04440
Philipp Spohr <philipp.spohr@hhu.de>
2021-02-01 13:26:22 +0100

    playing with het evaluation plots

---
+++ scripts/hetPlot1.py
@@ -0,0 +1,29 @@
+import sys
+import altair as alt
+import pandas as pd
+import vcfpy
+
+tuples = []
+
+ambiguousLetters = ['R','Y','S','W','K','M']
+
+for f in snakemake.input:
+    reader = vcfpy.Reader.from_path(f)
+    for record in reader:
+        isHet = False
+        for l in ambiguousLetters:
+            if l in record.ALT:
+                isHet = True
+                break
+        if isHet:
+            tuples.append((f,record.POS,record.INFO['RVT']))
+
+df = pd.DataFrame(tuples,columns=['file','pos','rvt'])
+
+chart = alt.Chart(df).mark_rect().encode(
+    y = 'pos:O',
+    x = 'file:N',
+    color= 'rvt:Q'
+).transform_filter(
+    selection
+).interactive().save(snakemake.output[0])
