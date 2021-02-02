0325a8dff57bbbf6200f225f159aa230c3ee5b63
Philipp Spohr <philipp.spohr@hhu.de>
2021-02-02 13:37:41 +0100

    add diffs and medians

--- scripts/hetPlot1.py
+++ scripts/hetPlot1.py
@@ -4,15 +4,17 @@ import pandas as pd
 import vcfpy
 from shared import *
 import os
+from functools import reduce
 
 tuples = []
-
+difftuples = []
 cutoffHeterozigosity = 0.1
 
 iterator = iter(snakemake.input)
 
 for pancovF, ivarF,nanoporeF,pileupF in zip(snakemake.input["pancov"], snakemake.input["ivar"],snakemake.input['nanopore'],snakemake.input['illuminaPileups']):
 
+    localtuples = []
 
     #We will store all positions that are in any of the vcfs/tables here and later add the illumina pileups
     pileupPositions = {}
@@ -24,7 +26,7 @@ for pancovF, ivarF,nanoporeF,pileupF in zip(snakemake.input["pancov"], snakemake
         alleleFrequency = record.INFO["VCOV"] / (
             record.INFO["VCOV"] + record.INFO["RCOV"]
         )
-        tuples.append((pancovF, record.POS, "pancov", alleleFrequency,record.ALT[0].value))
+        localtuples.append((pancovF, record.POS, "pancov", alleleFrequency,record.ALT[0].value))
         pileupPositions[record.POS] = record.ALT[0].value
 
     #Nanopolish
@@ -32,7 +34,7 @@ for pancovF, ivarF,nanoporeF,pileupF in zip(snakemake.input["pancov"], snakemake
     for record in reader:
 
         alleleFrequency = record.INFO["SupportFraction"]
-        tuples.append((pancovF, record.POS, "nanopolish", alleleFrequency,record.ALT[0].value))
+        localtuples.append((pancovF, record.POS, "nanopolish", alleleFrequency,record.ALT[0].value))
         pileupPositions[record.POS] = record.ALT[0].value
 
     #Ajvar
@@ -43,8 +45,9 @@ for pancovF, ivarF,nanoporeF,pileupF in zip(snakemake.input["pancov"], snakemake
         pos = d[1]
         altallele = d[3]
         illuminafreq = float(d[10])
-        tuples.append((pancovF, pos, "ivar", illuminafreq,altallele))
+        localtuples.append((pancovF, pos, "ivar", illuminafreq,altallele))
         pileupPositions[pos] = altallele
+
 
     #Add Pileups
     pileup = parsePileupStrandAwareLight(pileupF)
@@ -52,28 +55,53 @@ for pancovF, ivarF,nanoporeF,pileupF in zip(snakemake.input["pancov"], snakemake
         ipos = int(pos)
         if ipos in pileup:
             af = getAlleleFrequency(pileup[ipos],pileupPositions[pos])
-            tuples.append((pancovF, pos, "illumina", af,'none'))
+            if snakemake.config['thresholdHomCall'] < af < 1-snakemake.config['thresholdHomCall']:
+                localtuples.append((pancovF, pos, "illumina", af,'none'))
+                for tuple in filter(lambda x: x[1] == pos and x[2] != 'illumina',localtuples):
+                    difftuples.append((tuple[0],tuple[2],tuple[3]-af))
+            else:
+                localtuples = filter(lambda x : x[1] != pos,localtuples)
         else:
             pass
             #print(ipos)
     #print('added {} potential het sites for file: {}'.format(len(pileupPositions),pancovF))
+    tuples += localtuples
 
 df = pd.DataFrame(tuples, columns=["file", "pos", "method", "alleleFreq","call"])
+diff_df = pd.DataFrame(difftuples,columns=["file","method","diff"])
 
-charts = []
 os.makedirs(snakemake.output[0],exist_ok=True)
 for f in df["file"].unique():
 
     outfile = f.split('.')[-2].split('/')
     outfile = outfile[-3]+'_'+outfile[-2]
-    charts.append(
 
-        alt.Chart(df[df.file == f],title=f).mark_bar().encode(
-            x="method:N",
-            y=alt.X("alleleFreq:Q", scale=alt.Scale(domain=[0, 1])),
-            color="method:N",
-            column="pos:O",
-            tooltip=["alleleFreq","call"],
-        ).interactive().save(os.path.join(snakemake.output[0],outfile+'.html'))
+    plots = []
 
+    plots.append( alt.Chart(df[df.file == f],title=f).mark_bar().encode(
+        x="method:N",
+        y=alt.X("alleleFreq:Q", scale=alt.Scale(domain=[0, 1])),
+        color="method:N",
+        column="pos:O",
+        tooltip=["alleleFreq","call"],
     )
+    )
+
+    for m in ['pancov','ivar','nanopolish']:
+        base = alt.Chart(diff_df[diff_df.file == f],title=m)
+
+        bar = base.mark_bar().encode(
+            x=alt.X('diff:Q', bin=True, axis=None),
+            y='count()'
+        )
+
+        rule = base.mark_rule(color='red').encode(
+            x='mean(diff):Q',
+            size=alt.value(5)
+        )
+
+        plots.append(bar + rule)
+
+
+    reduce(alt.vconcat,plots).interactive().save(os.path.join(snakemake.output[0], outfile + '.html'))
+    break
