e159774a5e452fc90ebf13c4792e94f07ad7c10f
Philipp Spohr <philipp.spohr@hhu.de>
2021-02-11 10:34:25 +0100

    count amb separately for eval

--- scripts/aggregateRecoveries.py
+++ scripts/aggregateRecoveries.py
@@ -2,6 +2,7 @@
 with open(snakemake.output[0],'w') as outfile:
 	total = 0
 	recoveredCount = 0
+	ambiguous = 0
 	for f in snakemake.input:
 		outfile.write(f+'\n')
 		with open(f,'r') as infile:
@@ -10,11 +11,14 @@ with open(snakemake.output[0],'w') as outfile:
 				outfile.write(l+'\n')
 				d = l.split()
 				recovered = eval(d[3])
+				altallele = d[2]
 				if recovered == -1:
 					continue
 				elif recovered == True:
 					recoveredCount += 1
+				if d[2] in ['R', 'Y', 'S', 'W', 'K', 'M']:
+					ambiguous += 1
 				total += 1
 
-	outfile.write('recovered {} of {} found variants'.format(recoveredCount,total))
+	outfile.write('recovered {} of {} found variants ({} are ambiguous characters)'.format(recoveredCount,total,ambiguous))
 
