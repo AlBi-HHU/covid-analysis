828a10a6c06b75ec5caf7040c337975f9b395d74
Pierre Marijon <pierre.marijon@hhu.de>
2021-02-22 15:58:46 +0100

    Add Nanopore drop of coverage in recovery stats

--- scripts/aggregateRecoveries.py
+++ scripts/aggregateRecoveries.py
@@ -1,24 +1,32 @@
-
-with open(snakemake.output[0],'w') as outfile:
+with open(snakemake.output[0], "w") as outfile:
     total = 0
     recoveredCount = 0
+    nanoDropCount = 0
     ambiguous = 0
     for f in snakemake.input:
-        outfile.write(f+'\n')
-        with open(f,'r') as infile:
+        outfile.write(f + "\n")
+        with open(f, "r") as infile:
             ll = infile.read().splitlines()[1:]
             for l in ll:
-                outfile.write(l+'\n')
+                outfile.write(l + "\n")
                 d = l.split()
-                recovered = eval(d[3])
+
+                recovered = d[3]
                 altallele = d[2]
-                if recovered == -1:
+
+                if recovered == "Reject":
                     continue
-                elif recovered == True:
+                elif recovered == "True":
                     recoveredCount += 1
-                if d[2] in ['R', 'Y', 'S', 'W', 'K', 'M']:
+                elif recovered == "Nanopore":
+                    nanoDropCount += 1
+
+                if d[2] in ["R", "Y", "S", "W", "K", "M"]:
                     ambiguous += 1
                 total += 1
 
-    outfile.write('recovered {} of {} found variants ({} are ambiguous characters)'.format(recoveredCount,total,ambiguous))
-
+    outfile.write(
+        "recovered {} of {} found variants ({} are ambiguous characters, {} nanopore drop of coverage)".format(
+            recoveredCount, total, ambiguous, nanoDropCount
+        )
+    )
--- scripts/illuminadiff2.py
+++ scripts/illuminadiff2.py
@@ -2,76 +2,120 @@ from shared import *
 import sys
 
 ambiguityChars = {
-	'R' : frozenset(('A', 'G')),
-	'Y' : frozenset(('C', 'T')),
-	'S' : frozenset(('G', 'C')),
-	'W' : frozenset(('A', 'T')),
-	'K' : frozenset(('T', 'G')),
-	'M' : frozenset(('A', 'C'))
+    "R": frozenset(("A", "G")),
+    "Y": frozenset(("C", "T")),
+    "S": frozenset(("G", "C")),
+    "W": frozenset(("A", "T")),
+    "K": frozenset(("T", "G")),
+    "M": frozenset(("A", "C")),
 }
 
-illuminapileup = parsePileupStrandAwareLight(snakemake.input['illuminaPileup'])
-nanoporepileup = parsePileupStrandAwareLight(snakemake.input['nanoporePileup'])
+illuminapileup = parsePileupStrandAwareLight(snakemake.input["illuminaPileup"])
+nanoporepileup = parsePileupStrandAwareLight(snakemake.input["nanoporePileup"])
 
-pancovInfoFile= open(snakemake.input['pancovInfo'], 'r').read().splitlines()
+pancovInfoFile = open(snakemake.input["pancovInfo"], "r").read().splitlines()
 
-with open(snakemake.output['diffFile'],'w') as outFile,open(snakemake.input['iVarInfo'],'r') as ivarInfoFile:
-	outFile.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format('Position','Ref','Alt','Recovered','Comment','Pileup Illu','Pileup Nano'))
+with open(snakemake.output["diffFile"], "w") as outFile, open(
+    snakemake.input["iVarInfo"], "r"
+) as ivarInfoFile:
+    outFile.write(
+        "{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+            "Position",
+            "Ref",
+            "Alt",
+            "Recovered",
+            "Comment",
+            "Pileup Illu",
+            "Pileup Nano",
+        )
+    )
 
-	for l in ivarInfoFile.read().splitlines():
-		lineData = l.split()
-		position = int(lineData[0])
-		reference = lineData[1]
-		altallele_unmodified = lineData[2]
-		altalleles = [altallele_unmodified]
+    for l in ivarInfoFile.read().splitlines():
+        lineData = l.split()
+        position = int(lineData[0])
+        reference = lineData[1]
+        altallele_unmodified = lineData[2]
+        altalleles = [altallele_unmodified]
 
-		if altallele_unmodified == 'N':
-			continue
-		if altallele_unmodified in ambiguityChars:
-			altalleles = list(ambiguityChars[altallele_unmodified]-{reference})
+        if altallele_unmodified == "N":
+            continue
+        if altallele_unmodified in ambiguityChars:
+            altalleles = list(ambiguityChars[altallele_unmodified] - {reference})
 
-		if position in illuminapileup:
-			for altallele in altalleles:
-				#Alex perl
-				sb = getStrandBias(illuminapileup[position],altallele)
-				cov = getCoverage(illuminapileup[position],altallele)
-				abs = getMinorStrandAbs(illuminapileup[position],altallele)
-				fq = getMinorStrandFrequency(illuminapileup[position],altallele)
+        if position in illuminapileup:
+            for altallele in altalleles:
+                # Alex perl
+                sb = getStrandBias(illuminapileup[position], altallele)
+                cov = getCoverage(illuminapileup[position], altallele)
+                abs = getMinorStrandAbs(illuminapileup[position], altallele)
+                fq = getMinorStrandFrequency(illuminapileup[position], altallele)
 
-				reject = False
-				if cov <= 10:
-					reject = True
-				elif cov <= 20:
-					if abs < 5:
-						reject = True
-				elif cov <= 50:
-					if abs < 10 and fq < 0.25:
-						reject = True
-				elif cov <= 100:
-					if abs < 15 and fq < 0.15:
-						reject = True
-				else:
-					if fq < 0.1:
-						reject = True
-				if reject:
-					outFile.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(position,reference,altallele_unmodified,-1,'Allele Component: {} did not pass Alex Perl Filter, we ignore it'.format(altallele),illuminapileup[position],
-					                                                    nanoporepileup[position] if position in nanoporepileup else 'No nanopore pileup'
-					                                                    ))
-					break
-			else:
-				recovered = False
-				for l2 in pancovInfoFile:
-					#print(l,l2)
-					lineData2 = l2.split()
-					position2 = int(lineData2[0])
-					reference2 = lineData2[1]
-					altallele2 = lineData2[2]
-					if position2 == position and altallele2 == altallele_unmodified:
-						recovered = True
-						break
-				outFile.write('{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(position,reference,altallele_unmodified,recovered,'',illuminapileup[position],
-				                                                    nanoporepileup[position] if position in nanoporepileup else 'No nanopore pileup'
-				                                                    ))
-		else:
-			print('Position {} not covered by illumina pileup (but called in ivar, this is fishy)'.format(position))
-			sys.exit(-1)
+                reject = False
+                if cov <= 10:
+                    reject = True
+                elif cov <= 20:
+                    if abs < 5:
+                        reject = True
+                elif cov <= 50:
+                    if abs < 10 and fq < 0.25:
+                        reject = True
+                elif cov <= 100:
+                    if abs < 15 and fq < 0.15:
+                        reject = True
+                else:
+                    if fq < 0.1:
+                        reject = True
+
+                if reject:
+                    outFile.write(
+                        "{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+                            position,
+                            reference,
+                            altallele_unmodified,
+                            "Reject",
+                            "Allele Component: {} did not pass Alex Perl Filter, we ignore it".format(
+                                altallele
+                            ),
+                            illuminapileup[position],
+                            nanoporepileup[position]
+                            if position in nanoporepileup
+                            else "No nanopore pileup",
+                        )
+                    )
+                    break
+            else:
+                recovered = "False"
+
+                for l2 in pancovInfoFile:
+                    # print(l,l2)
+                    lineData2 = l2.split()
+                    position2 = int(lineData2[0])
+                    reference2 = lineData2[1]
+                    altallele2 = lineData2[2]
+                    if position2 == position and altallele2 == altallele_unmodified:
+                        recovered = "True"
+                        break
+
+                if position in nanoporepileup and sum(nanoporepileup[position].values()) < snakemake.config["nanoporeCoverageCutoff"]:
+                    recovered = "Nanopore"
+
+                outFile.write(
+                    "{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+                        position,
+                        reference,
+                        altallele_unmodified,
+                        recovered,
+                        "",
+                        illuminapileup[position],
+                        nanoporepileup[position]
+                        if position in nanoporepileup
+                        else "No nanopore pileup",
+                    )
+                )
+        else:
+            print(
+                "Position {} not covered by illumina pileup (but called in ivar, this is fishy)".format(
+                    position
+                )
+            )
+            sys.exit(-1)
