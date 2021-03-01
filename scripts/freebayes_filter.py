e6d2a9372f20e10490408cf832d79b9e6d609cbf
Pierre Marijon <pierre.marijon@hhu.de>
2021-03-01 10:33:27 +0100

    Replace shanon entropy homopolymer filter by same base range

--- scripts/freebayes_filter.py
+++ scripts/freebayes_filter.py
@@ -14,25 +14,32 @@ def main(vcf, entropy_th, ratio_ref_cov_th, out):
         coverage = record.INFO["DP"]
         ref_cov = record.INFO["RO"]
         ratio = ref_cov / coverage
-        
-        entropy = compute_entropy(record.REF)
 
-        if (entropy < 0.001 or entropy > entropy_th) and ratio < ratio_ref_cov_th:
-            
+        seqs = [record.REF]
+        seqs.extend([alt.serialize() for alt in record.ALT])
+        homo = any([is_homopolymer(seq) for seq in seqs])
+
+        if not homo and ratio < ratio_ref_cov_th:
             writer.write_record(record)
 
-            
-def compute_entropy(seq):
-    entropy = 0
-    for k, v in Counter(seq).items():
-        entropy += v/len(seq) * log2(v/len(seq))
 
-    return entropy * -1
+def is_homopolymer(seq):
+    successive_base = 1
+    for b, c in zip(seq, seq[1:]):
+        if b == c:
+            successive_base += 1
+        else:
+            successive_base = 1
 
-    
+        if successive_base >= 4:
+            return True
+
+    return False
+
+
 if "snakemake" in locals():
     main(snakemake.input["vcf"], snakemake.params["e_th"], snakemake.params["ratio_ref_cov"], snakemake.output["output"])
 else:
     import sys
-    
+
     main(sys.argv[1], float(sys.argv[2]), float(sys.argv[3]), sys.argv[4])
