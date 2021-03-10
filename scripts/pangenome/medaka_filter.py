16c08574bd8049251e1c0941b9cd289a8938c5cb
Pierre Marijon <pierre.marijon@hhu.de>
2021-03-10 11:39:44 +0100

    Fix typo in medaka_filter

--- scripts/pangenome/medaka_filter.py
+++ scripts/pangenome/medaka_filter.py
@@ -15,26 +15,21 @@ def main(inputs, output):
             record.FORMAT = dict()
             nano_var[key] = record
 
-    header = vcfpy.Header(samples=vcfpy.header.SamplesInfos(["all_var"]))
-    # header = vcfpy.Header()
-    header.add_line(vcfpy.HeaderLine("fileformat", "VCFv4.2"))
-    header.add_contig_line({"ID": "MN908947.3", "length": "29903"})
-    header.add_filter_line({"Description": "All filters passed", "ID": "PASS"})
-    header.add_filter_line({"Description": "dp", "ID": "dp"})
-
-    writer = vcfpy.Writer.from_path(output, header)
-
     reader = vcfpy.Reader.from_path(inputs["medaka"])
-    for vcf in reader:
+
+    writer = vcfpy.Writer.from_path(output, reader.header)
+
+    for record in reader:
         key = (
             str(record.POS)
             + record.REF
             + "".join([a.serialize() for a in record.ALT])
         )
+
         if any([alt.type == "DEL" for alt in record.ALT]) and nano_var is not None and key not in nano_var:
             continue
 
-        if record.INFO["AQ"] < config["medakaQualityThreshold"]:
+        if record.INFO["AQ"] < snakemake.config["medakaQualityThreshold"]:
             continue
 
         writer.write_record(record)
