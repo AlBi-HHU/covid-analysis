106cf0777db83e68b6528593a11fa4741e53c790
Pierre Marijon <pierre.marijon@hhu.de>
2021-04-15 14:12:19 +0200

    Add alex strand biais filter in freebayes filter

--- scripts/freebayes_filter.py
+++ scripts/freebayes_filter.py
@@ -1,6 +1,6 @@
 import vcfpy
 
-from shared import compute_entropy
+from shared import compute_entropy, alexSBFilter
 
 
 def main(vcf, entropy_th, ratio_ref_cov_th, out):
@@ -13,9 +13,17 @@ def main(vcf, entropy_th, ratio_ref_cov_th, out):
         ref_cov = record.INFO["RO"]
         ratio = ref_cov / coverage
 
+        var_f = int(record.INFO["SAF"][0])
+        var_r = int(record.INFO["SRF"])
+
+        cov = var_f + var_r
+        mincov = min(var_f, var_r)
+        fq = mincov / cov
+        minfq = min(1 - fq, fq)
+
         entropy = compute_entropy(record.REF)
 
-        if (entropy < 0.001 or entropy > entropy_th) and ratio < ratio_ref_cov_th:
+        if (entropy < 0.001 or entropy > entropy_th) and ratio < ratio_ref_cov_th and not alexSBFilter(mincov, cov, minfq):
 
             writer.write_record(record)
 
