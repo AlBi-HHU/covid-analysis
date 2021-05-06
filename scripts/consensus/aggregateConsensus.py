0e531056bbe2600314d6a096525899c5104cd381
Philipp Spohr <philipp.spohr@hhu.de>
2021-05-06 15:15:01 +0200

    parameterized aggregation function

--- scripts/consensus/aggregateConsensus.py
+++ scripts/consensus/aggregateConsensus.py
@@ -3,8 +3,14 @@ from Bio import SeqIO
 with open(snakemake.output[0], "w") as outfile:
     records = []
 
-    for f in snakemake.input:
+    for string in snakemake.params['input']:
+        data = string.split('<::>')
+        f = data[0]
+        run = data[1]
+        barcode = data[2]
         rec = SeqIO.read(f, "fasta")
-        rec.id = f
+        rec.id = run + '_' + barcode
+        rec.description = ''
+        rec.name = ''
         records.append(rec)
     SeqIO.write(records, outfile, "fasta")
